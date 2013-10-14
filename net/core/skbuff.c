39ef0a7972c10e1673e01e4b0ada240a03bc53c8
Andrey Konovalov <andreyknvl@google.com>
2013-10-14 11:58:40 +0400

    Small fix

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1066,7 +1066,7 @@ int pskb_expand_head(struct sk_buff *skb, int nhead, int ntail,
 			       gfp_mask, NUMA_NO_NODE, NULL);
 	if (!data)
 		goto nodata;
-	size = SKB_WITH_OVERHEAD(/*ksize(data)*/size);
+	size = SKB_WITH_OVERHEAD(/*ksize(data)*/size + SKB_DATA_ALIGN(sizeof(struct skb_shared_info)));
 
 	/* Copy only real data... and, alas, header. This should be
 	 * optimized for the cases when header is void.
