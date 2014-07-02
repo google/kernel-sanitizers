f15ff6c86aa5b2e56f07684a09668530b2b85448
Hugh Dickins <hughd@google.com>
2014-07-03 08:43:25 +1000

    mm-memcontrol-rewrite-charge-api-fix-shmem_unuse-fix
    Add comments on the private use of -EAGAIN.
    
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -594,7 +594,7 @@ static int shmem_unuse_inode(struct shmem_inode_info *info,
 	radswap = swp_to_radix_entry(swap);
 	index = radix_tree_locate_item(&mapping->page_tree, radswap);
 	if (index == -1)
-		return -EAGAIN;
+		return -EAGAIN;	/* tell shmem_unuse we found nothing */
 
 	/*
 	 * Move _head_ to start search for next from here.
@@ -695,6 +695,7 @@ int shmem_unuse(swp_entry_t swap, struct page *page)
 		cond_resched();
 		if (error != -EAGAIN)
 			break;
+		/* found nothing in this: move on to search the next */
 	}
 	mutex_unlock(&shmem_swaplist_mutex);
 
