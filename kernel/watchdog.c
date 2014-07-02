63883f68ece12eeb2df01e8432d4f1c90b54f87c
Josh Hunt <johunt@akamai.com>
2014-07-03 08:44:01 +1000

    panic: add TAINT_SOFTLOCKUP
    This taint flag will be set if the system has ever entered a softlockup
    state.  Similar to TAINT_WARN it is useful to know whether or not the
    system has been in a softlockup state when debugging.
    
    Signed-off-by: Josh Hunt <johunt@akamai.com>
    Cc: Jason Baron <jbaron@akamai.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -370,6 +370,7 @@ static enum hrtimer_restart watchdog_timer_fn(struct hrtimer *hrtimer)
 
 		if (softlockup_panic)
 			panic("softlockup: hung tasks");
+		add_taint(TAINT_SOFTLOCKUP, LOCKDEP_STILL_OK);
 		__this_cpu_write(soft_watchdog_warn, true);
 	} else
 		__this_cpu_write(soft_watchdog_warn, false);
