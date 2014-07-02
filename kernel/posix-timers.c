c942c6aae53bf138f8f40968b66b1bb8dfdff424
Andrew Morton <akpm@linux-foundation.org>
2014-07-03 08:43:03 +1000

    kernel-posix-timersc-code-clean-up-checkpatch-fixes
    WARNING: space prohibited between function name and open parenthesis '('
    #55: FILE: kernel/posix-timers.c:345:
    +					       sizeof (struct k_itimer), 0,
    
    ERROR: do not use assignment in if condition
    #70: FILE: kernel/posix-timers.c:504:
    +	if ((event->sigev_notify & SIGEV_THREAD_ID) &&
    
    total: 1 errors, 1 warnings, 192 lines checked
    
    ./patches/kernel-posix-timersc-code-clean-up.patch has style problems, please review.
    
    If any of these errors are false positives, please report
    them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: Fabian Frederick <fabf@skynet.be>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/posix-timers.c
+++ kernel/posix-timers.c
@@ -342,7 +342,7 @@ static __init int init_posix_timers(void)
 	posix_timers_register_clock(CLOCK_TAI, &clock_tai);
 
 	posix_timers_cache = kmem_cache_create("posix_timers_cache",
-					       sizeof (struct k_itimer), 0,
+					       sizeof(struct k_itimer), 0,
 					       SLAB_PANIC, NULL);
 	return 0;
 }
