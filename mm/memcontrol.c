608a88d883dbd66253c3a0778a21fe2701bce713
Michal Hocko <mhocko@suse.cz>
2014-07-03 08:44:19 +1000

    memcg: deprecate memory.force_empty knob
    force_empty has been introduced primarily to drop memory before it gets
    reparented on the group removal.  This alone doesn't sound fully justified
    because reparented pages which are not in use can be reclaimed also later
    when there is a memory pressure on the parent level.
    
    Mark the knob CFTYPE_INSANE which tells the cgroup core that it shouldn't
    create the knob with the experimental sane_behavior.  Other users will get
    informed about the deprecation and asked to tell us more because I do not
    expect most users will use sane_behavior cgroups mode very soon.
    
    Anyway I expect that most users will be simply cgroup remove handlers
    which do that since ever without having any good reason for it.
    
    If somebody really cares because reparented pages, which would be dropped
    otherwise, push out more important ones then we should fix the reparenting
    code and put pages to the tail.
    
    Signed-off-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Greg Thelen <gthelen@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4135,6 +4135,10 @@ static ssize_t mem_cgroup_force_empty_write(struct kernfs_open_file *of,
 
 	if (mem_cgroup_is_root(memcg))
 		return -EINVAL;
+	pr_info("%s (%d): memory.force_empty is deprecated and will be removed.",
+			current->comm, task_pid_nr(current));
+	pr_cont(" Let us know if you know if it needed in your usecase at");
+	pr_cont(" linux-mm@kvack.org\n");
 	return mem_cgroup_force_empty(memcg) ?: nbytes;
 }
 
@@ -5311,6 +5315,7 @@ static struct cftype mem_cgroup_files[] = {
 	},
 	{
 		.name = "force_empty",
+		.flags = CFTYPE_INSANE,
 		.write = mem_cgroup_force_empty_write,
 	},
 	{
