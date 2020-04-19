4d74ec67c32f04c414592d5b6d8cd8ff95bae904
Alexander Potapenko <glider@google.com>
2020-04-19 14:27:29 +0200

    kfence: support non-kmalloc caches
    Keep an array of struct kmem_cache instances from which we can steal a
    freelist. Such caches are registered upon creation unless they have a
    constructer, the SLAB_TYPESAFE_BY_RCU flag, or their object size is
    greater then one page.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- include/linux/kfence.h
+++ include/linux/kfence.h
@@ -15,6 +15,10 @@ void *kfence_alloc_and_fix_freelist(struct kmem_cache *s, gfp_t gfp);
 bool kfence_free(struct kmem_cache *s, struct page *page, void *head,
 		 void *tail, int cnt, unsigned long addr);
 
+void kfence_cache_register(struct kmem_cache *s);
+
+void kfence_cache_unregister(struct kmem_cache *s);
+
 size_t kfence_ksize(void *object);
 
 bool kfence_handle_page_fault(unsigned long address);
@@ -33,6 +37,15 @@ static inline bool kfence_free(struct kmem_cache *s, struct page *page,
 {
 	return false;
 }
+
+static void kfence_cache_register(struct kmem_cache *s)
+{
+}
+
+static void kfence_cache_unregister(struct kmem_cache *s)
+{
+}
+
 static size_t kfence_ksize(void *object)
 {
 	return 0;
