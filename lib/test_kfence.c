c7078f40ae2e1312086a28c2408090a9211336c4
Alexander Potapenko <glider@google.com>
2020-05-01 18:50:12 +0200

    test_kfence.c: add more tests (use caches where possible)
    Check OOB and UAF errors with both kmalloc() and custom caches, so that
    we can test memcg support as well.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- lib/test_kfence.c
+++ lib/test_kfence.c
@@ -23,27 +23,56 @@
  */
 #define MAX_ITER 2000
 
+/* Cache used by tests. If empty, allocate from kmalloc instead. */
+static struct kmem_cache *current_cache;
+
+static bool setup_cache(size_t size)
+{
+	/*
+	 * Use SLAB_NOLEAKTRACE to prevent merging this cache with existing
+	 * caches. Any other flag from SLAB_NEVER_MERGE except
+	 * SLAB_TYPESAFE_BY_RCU (which disables KFENCE for a cache) would also
+	 * work.
+	 * Use SLAB_ACCOUNT to allocate via memcg, if enabled.
+	 */
+	current_cache = kmem_cache_create(
+		"test_cache", size, 1, SLAB_NOLEAKTRACE | SLAB_ACCOUNT, NULL);
+	if (!current_cache)
+		return false;
+	return true;
+}
+
+static void teardown_cache(void)
+{
+	kmem_cache_destroy(current_cache);
+	current_cache = NULL;
+}
+
+static void free_to_kfence(void *ptr)
+{
+	if (!current_cache)
+		kfree(ptr);
+	else
+		kmem_cache_free(current_cache, ptr);
+}
+
 /*
- * Allocate using either kmalloc or the given memory cache till we get an object
- * from KFENCE pool or hit the maximum number of attempts.
+ * Allocate using either kmalloc or the currently used memory cache till we get
+ * an object from KFENCE pool or hit the maximum number of attempts.
  */
-static void *alloc_from_kfence(struct kmem_cache *s, size_t size, gfp_t gfp,
-			       const char *caller)
+static void *alloc_from_kfence(size_t size, gfp_t gfp, const char *caller)
 {
 	void *res;
 	int i;
 
 	for (i = 0; i < MAX_ITER; i++) {
-		if (!s)
+		if (!current_cache)
 			res = kmalloc(size, gfp);
 		else
-			res = kmem_cache_alloc(s, gfp);
+			res = kmem_cache_alloc(current_cache, gfp);
 		if (is_kfence_ptr((unsigned long)res))
 			return res;
-		if (!s)
-			kfree(res);
-		else
-			kmem_cache_free(s, res);
+		free_to_kfence(res);
 		/* TODO: sleep time depends on heartbeat period. */
 		msleep(100);
 	}
@@ -51,65 +80,80 @@ static void *alloc_from_kfence(struct kmem_cache *s, size_t size, gfp_t gfp,
 	return NULL;
 }
 
-static int do_test_oob(size_t size)
+static int do_test_oob(size_t size, bool use_cache)
 {
 	void *buffer;
 	char *c;
+	int res = 0;
 
-	buffer = alloc_from_kfence(NULL, size, GFP_KERNEL, __func__);
-	if (!buffer)
-		return 1;
-	/* We will hit KFENCE redzone at one of the buffer's ends. */
-	c = ((char *)buffer) + size + 1;
-	READ_ONCE(*c);
-	c = ((char *)buffer) - 1;
-	READ_ONCE(*c);
-	kfree(buffer);
-	return 0;
+	if (use_cache)
+		if (!setup_cache(size))
+			return 1;
+	buffer = alloc_from_kfence(size, GFP_KERNEL, __func__);
+	if (buffer) {
+		/* We will hit KFENCE redzone at one of the buffer's ends. */
+		c = ((char *)buffer) + size + 1;
+		READ_ONCE(*c);
+		c = ((char *)buffer) - 1;
+		READ_ONCE(*c);
+		free_to_kfence(buffer);
+	} else {
+		res = 1;
+	}
+	if (use_cache)
+		teardown_cache();
+	return res;
 }
 
-static int do_test_uaf(size_t size)
+static int do_test_uaf(size_t size, bool use_cache)
 {
 	void *buffer;
 	char *c;
+	int res = 0;
 
-	buffer = alloc_from_kfence(NULL, size, GFP_KERNEL, __func__);
-	if (!buffer)
-		return 1;
-	c = (char *)buffer;
-	kfree(buffer);
-	READ_ONCE(*c);
-	return 0;
+	if (use_cache)
+		if (!setup_cache(size))
+			return 1;
+	buffer = alloc_from_kfence(size, GFP_KERNEL, __func__);
+	if (buffer) {
+		c = (char *)buffer;
+		free_to_kfence(buffer);
+		READ_ONCE(*c);
+	} else {
+		res = 1;
+	}
+	if (use_cache)
+		teardown_cache();
+	return res;
 }
 
 /* Test cache creation, shrinking and destroying with KFENCE. */
 static int do_test_shrink(int size)
 {
-	struct kmem_cache *c;
 	void *buffer;
+	int res = 0;
 
-	/*
-	 * Use SLAB_NOLEAKTRACE to prevent merging this cache with existing
-	 * caches. Any other flag from SLAB_NEVER_MERGE except
-	 * SLAB_TYPESAFE_BY_RCU (which disables KFENCE for a cache) would also
-	 * work.
-	 */
-	c = kmem_cache_create("test_cache", size, 1, SLAB_NOLEAKTRACE, NULL);
-	buffer = alloc_from_kfence(c, size, GFP_KERNEL, __func__);
-	if (!buffer)
+	if (!setup_cache(size))
 		return 1;
-	kmem_cache_shrink(c);
-	kmem_cache_free(c, buffer);
-	kmem_cache_destroy(c);
-	return 0;
+	buffer = alloc_from_kfence(size, GFP_KERNEL, __func__);
+	if (buffer) {
+		kmem_cache_shrink(current_cache);
+		free_to_kfence(buffer);
+	} else {
+		res = 1;
+	}
+	teardown_cache();
+	return res;
 }
 
 static int __init test_kfence_init(void)
 {
 	int failures = 0;
 
-	failures += do_test_oob(32);
-	failures += do_test_uaf(32);
+	failures += do_test_oob(32, false);
+	failures += do_test_oob(32, true);
+	failures += do_test_uaf(32, false);
+	failures += do_test_uaf(32, true);
 	failures += do_test_shrink(32);
 
 	if (failures == 0)
