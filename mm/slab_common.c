0d67ee1ea1bef63a3470d4b089e2667ea7231079
Alexander Potapenko <glider@google.com>
2020-06-10 13:57:14 +0200

    kfence: implement kfence_ksize()
    For kfence-allocated objects, return the actual available size for the
    case someone is going to use it.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1771,7 +1771,10 @@ size_t ksize(const void *objp)
 	if (unlikely(objp == ZERO_SIZE_PTR) || !__kasan_check_read(objp, 1))
 		return 0;
 
-	size = __ksize(objp);
+	size = kfence_ksize(objp);
+
+	if (!size)
+		size = __ksize(objp);
 	/*
 	 * We assume that ksize callers could use whole allocated area,
 	 * so we need to unpoison this area.
