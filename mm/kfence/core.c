393b800cfecb7435114bfb938fefa473c2ea8c79
Alexander Potapenko <glider@google.com>
2020-07-15 15:26:49 +0200

    kfence: fold static_key.c into core.c
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- mm/kfence/core.c
+++ mm/kfence/core.c
@@ -372,6 +372,14 @@ static void for_each_canary(int index, void (*fn)(unsigned long))
 		fn(addr);
 }
 
+/* The static key to set up a KFENCE allocation. */
+DEFINE_STATIC_KEY_FALSE(kfence_allocation_key);
+
+/* Gates the allocation, ensuring only one succeeds in a given period. */
+static atomic_t allocation_gate = ATOMIC_INIT(1);
+/* Wait queue to wake up heartbeat timer task. */
+static DECLARE_WAIT_QUEUE_HEAD(allocation_wait);
+
 void *kfence_guarded_alloc(struct kmem_cache *cache, size_t override_size, gfp_t gfp)
 {
 	unsigned long flags;
@@ -423,6 +431,35 @@ void *kfence_guarded_alloc(struct kmem_cache *cache, size_t override_size, gfp_t
 	pr_debug("allocated object #%d\n", index);
 	return ret;
 }
+
+void *__kfence_alloc(struct kmem_cache *s, size_t size, gfp_t flags)
+{
+	void *ret;
+
+	/*
+	 * allocation_gate only needs to become non-zero, so it doesn't make
+	 * sense to continue writing to it and pay the associated contention
+	 * cost, in case we have a large number of concurrent allocations.
+	 */
+	if (atomic_read(&allocation_gate) || atomic_inc_return(&allocation_gate) > 1)
+		return NULL;
+	wake_up(&allocation_wait);
+
+	if (!kfence_is_enabled())
+		return NULL;
+
+	// TODO(elver): Remove one of the comparisons, which is redundant.
+	if ((size > PAGE_SIZE) || (s->size > PAGE_SIZE))
+		return NULL;
+	if (s->flags & SLAB_TYPESAFE_BY_RCU)
+		return NULL;
+
+	ret = kfence_guarded_alloc(s, size, flags);
+
+	return ret;
+}
+
+
 
 void kfence_guarded_free(void *addr)
 {
@@ -595,6 +632,30 @@ device_initcall(kfence_create_debugfs);
 
 unsigned long kfence_sample_rate = KFENCE_DEFAULT_SAMPLE_RATE;
 
+/*
+ * Set up delayed work, which will enable and disable the static key. We need to
+ * use a work queue (rather than a simple timer), since enabling and disabling a
+ * static key cannot be done from an interrupt.
+ */
+static struct delayed_work kfence_timer;
+static void kfence_heartbeat(struct work_struct *work)
+{
+	if (!kfence_is_enabled())
+		return;
+
+	/* Enable static key, and await allocation to happen. */
+	atomic_set(&allocation_gate, 0);
+	static_branch_enable(&kfence_allocation_key);
+	wait_event(allocation_wait, atomic_read(&allocation_gate) != 0);
+
+	/* Disable static key and reset timer. */
+	static_branch_disable(&kfence_allocation_key);
+	schedule_delayed_work(&kfence_timer, msecs_to_jiffies(kfence_sample_rate));
+}
+static DECLARE_DELAYED_WORK(kfence_timer, kfence_heartbeat);
+
+
+
 #ifdef MODULE_PARAM_PREFIX
 #undef MODULE_PARAM_PREFIX
 #endif
@@ -625,4 +686,12 @@ void __init kfence_init(void)
 			return;
 	}
 	pr_err("kfence_init failed\n");
+}
+
+/*
+ * TODO: naive implementation doesn't strictly require waiting for RNG anymore.
+ */
+void kfence_impl_init(void)
+{
+	schedule_delayed_work(&kfence_timer, 0);
 }
