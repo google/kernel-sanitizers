a243ee4efb7d2dc220a89df5162261d0add3ec96
Marco Elver <elver@google.com>
2020-07-24 23:23:32 +0200

    kfence: Fix kmem_cache_alloc()
    kcsan-test found that memcache allocations weren't properly guarded.
    This was due to passing the wrong size through to kfence.
    
    Signed-off-by: Marco Elver <elver@google.com>

--- mm/slub.c
+++ mm/slub.c
@@ -2852,7 +2852,7 @@ static __always_inline void *slab_alloc(struct kmem_cache *s,
 
 void *kmem_cache_alloc(struct kmem_cache *s, gfp_t gfpflags)
 {
-	void *ret = slab_alloc(s, gfpflags, _RET_IP_, s->size);
+	void *ret = slab_alloc(s, gfpflags, _RET_IP_, s->object_size);
 
 	trace_kmem_cache_alloc(_RET_IP_, ret, s->object_size,
 				s->size, gfpflags);
@@ -2875,7 +2875,7 @@ EXPORT_SYMBOL(kmem_cache_alloc_trace);
 #ifdef CONFIG_NUMA
 void *kmem_cache_alloc_node(struct kmem_cache *s, gfp_t gfpflags, int node)
 {
-	void *ret = slab_alloc_node(s, gfpflags, node, _RET_IP_, s->size);
+	void *ret = slab_alloc_node(s, gfpflags, node, _RET_IP_, s->object_size);
 
 	trace_kmem_cache_alloc_node(_RET_IP_, ret,
 				    s->object_size, s->size, gfpflags, node);
