4afc806a04a009a7b289566293864ffca9c73c94
Alexander Potapenko <glider@google.com>
2020-07-28 13:09:15 +0200

    x86, kfence: enable KFENCE for x86
    Add architecture specific implementation details for KFENCE and enable
    KFENCE for the x86 architecture. In particular, this implements the
    required interface in <asm/kfence.h> for setting up the pool and
    providing helper functions for protecting and unprotecting pages.
    
    For x86, we need to ensure that the pool uses 4K pages, which is done
    using the set_memory_4k() helper function.
    
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>
    Co-developed-by: Marco Elver <elver@google.com>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Alexander Potapenko <glider@google.com>
    ---
    v5:
    * MAJOR CHANGE: Switch to the memblock_alloc'd pool. Running benchmarks
      with the newly optimized is_kfence_address(), no difference between
      baseline and KFENCE is observed.
    * Suggested by Jann Horn:
      * Move x86 kfence_handle_page_fault before oops handling.
      * WARN_ON in kfence_protect_page if non-4K pages.
      * Better comments for x86 kfence_protect_page.
    
    v4:
    * Define __kfence_pool_attrs.

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -9,6 +9,7 @@
 #include <linux/kdebug.h>		/* oops_begin/end, ...		*/
 #include <linux/extable.h>		/* search_exception_tables	*/
 #include <linux/memblock.h>		/* max_low_pfn			*/
+#include <linux/kfence.h>		/* kfence_handle_page_fault	*/
 #include <linux/kprobes.h>		/* NOKPROBE_SYMBOL, ...		*/
 #include <linux/mmiotrace.h>		/* kmmio_handler, ...		*/
 #include <linux/perf_event.h>		/* perf_sw_event		*/
@@ -724,6 +725,9 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 	 */
 	if (IS_ENABLED(CONFIG_EFI))
 		efi_recover_from_page_fault(address);
+
+	if (kfence_handle_page_fault(address))
+		return;
 
 oops:
 	/*
