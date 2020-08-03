b39725eef736fcb9420f10fc76712f9cdfd52439
Marco Elver <elver@google.com>
2020-08-03 20:58:51 +0200

    kfence: Add ARM64 support
    Unfortunately, it seems that on ARM64, using a statically allocated pool
    does not work.
    
    In particular, virt_to_page() never returned a valid page. Is it
    possible that .bss and .data section on arm64 do not have valid pages?
    
    Signed-off-by: Marco Elver <elver@google.com>

---
+++ arch/arm64/include/asm/kfence.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __ASM_KFENCE_H
+#define __ASM_KFENCE_H
+
+#include <linux/kfence.h>
+#include <linux/log2.h>
+#include <linux/mm.h>
+
+#include <asm/cacheflush.h>
+
+#define KFENCE_POOL_ALIGNMENT PAGE_SIZE
+#define KFENCE_SKIP_ARCH_FAULT_HANDLER "el1_sync"
+
+static inline bool arch_kfence_initialize_pool(void)
+{
+	const unsigned int num_pages = ilog2(roundup_pow_of_two(KFENCE_POOL_SIZE / PAGE_SIZE));
+	struct page *pages = alloc_pages(GFP_KERNEL, num_pages);
+
+	if (!pages)
+		return false;
+
+	__kfence_pool = page_address(pages);
+	return true;
+}
+
+static inline bool kfence_protect_page(unsigned long addr, bool protect)
+{
+	set_memory_valid(addr, 1, !protect);
+
+	return true;
+}
+
+#endif /* __ASM_KFENCE_H */
