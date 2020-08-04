89d0f75486dc8ebce9c2c17f10e86e98d434211e
Alexander Potapenko <glider@google.com>
2020-08-04 18:59:13 +0200

    kfence, kasan: make KFENCE compatible with KASAN
    Make KFENCE compatible with KASAN. Currently this helps test KFENCE
    itself, where KASAN can catch potential corruptions to KFENCE state, or
    other corruptions that may be a result of freepointer corruptions in the
    main allocators.
    
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>
    Reviewed-by: Jann Horn <jannh@google.com>
    Co-developed-by: Marco Elver <elver@google.com>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Alexander Potapenko <glider@google.com>
    ---
    v7:
    * Remove EXPERT restriction for enabling KASAN+KFENCE. In future, MTE-based KASAN
      without stack traces will benefit from having KFENCE (which has stack
      traces). Removing EXPERT restriction allows this for production
      builds. The Kconfig help-text should still make it clear that in most
      cases KFENCE+KASAN does not make sense.
    * Also skip kasan_poison_shadow() if KFENCE object. It turns out that
      kernel/scs.c is a user of kasan_{poison,unpoison}_object_data().
    * Add Jann's Reviewed-by.
    
    v5:
    * Also guard kasan_unpoison_shadow with is_kfence_address(), as it may
      be called from SL*B internals, currently ksize().
    * Make kasan_record_aux_stack() compatible with KFENCE, which may be
      called from outside KASAN runtime.

--- mm/kasan/common.c
+++ mm/kasan/common.c
@@ -18,6 +18,7 @@
 #include <linux/init.h>
 #include <linux/kasan.h>
 #include <linux/kernel.h>
+#include <linux/kfence.h>
 #include <linux/kmemleak.h>
 #include <linux/linkage.h>
 #include <linux/memblock.h>
@@ -124,6 +125,10 @@ void kasan_poison_shadow(const void *address, size_t size, u8 value)
 	 */
 	address = reset_tag(address);
 
+	/* Skip KFENCE memory if called explicitly outside of sl*b. */
+	if (is_kfence_address(address))
+		return;
+
 	shadow_start = kasan_mem_to_shadow(address);
 	shadow_end = kasan_mem_to_shadow(address + size);
 
@@ -140,6 +145,14 @@ void kasan_unpoison_shadow(const void *address, size_t size)
 	 * addresses to this function.
 	 */
 	address = reset_tag(address);
+
+	/*
+	 * Skip KFENCE memory if called explicitly outside of sl*b. Also note
+	 * that calls to ksize(), where size is not a multiple of machine-word
+	 * size, would otherwise poison the invalid portion of the word.
+	 */
+	if (is_kfence_address(address))
+		return;
 
 	kasan_poison_shadow(address, size, tag);
 
@@ -396,6 +409,9 @@ static bool __kasan_slab_free(struct kmem_cache *cache, void *object,
 	tagged_object = object;
 	object = reset_tag(object);
 
+	if (is_kfence_address(object))
+		return false;
+
 	if (unlikely(nearest_obj(cache, virt_to_head_page(object), object) !=
 	    object)) {
 		kasan_report_invalid_free(tagged_object, ip);
@@ -443,6 +459,9 @@ static void *__kasan_kmalloc(struct kmem_cache *cache, const void *object,
 
 	if (unlikely(object == NULL))
 		return NULL;
+
+	if (is_kfence_address(object))
+		return (void *)object;
 
 	redzone_start = round_up((unsigned long)(object + size),
 				KASAN_SHADOW_SCALE_SIZE);
