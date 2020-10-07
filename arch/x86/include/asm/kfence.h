07629b66a85c05dc2465e83ad8ce9543c635d9f4
Marco Elver <elver@google.com>
2020-10-07 16:53:32 +0200

    kfence, x86: Switch to a dynamic pool
    Running benchmarks with the recently optimized is_kfence_address(), no
    difference between baseline and KFENCE is observed (defconfig + KFENCE
    defaults, sysbench I/O, 10 samples each, reboots between samples).
    
    Just for the record:
    
    	>>> baseline = [7152.41, # Requests/sec
    	... 7126.67,
    	... 7178.26,
    	... 6961.76,
    	... 7144.32,
    	... 7156.41,
    	... 6972.58,
    	... 7939.75,
    	... 7325.92,
    	... 7195.87]
    	>>> kfence = [7137.53, # Requests/sec
    	... 7284.93,
    	... 7040.96,
    	... 7381.09,
    	... 7341.08,
    	... 6993.57,
    	... 7840.23,
    	... 7205.52,
    	... 7944.94,
    	... 7086.45]
    	>>> numpy.mean(baseline); numpy.median(baseline)
    	7215.3949999999995
    	7154.41
    	>>> numpy.mean(kfence); numpy.median(kfence)
    	7325.63
    	7245.225
    
    Signed-off-by: Marco Elver <elver@google.com>

--- arch/x86/include/asm/kfence.h
+++ arch/x86/include/asm/kfence.h
@@ -11,8 +11,7 @@
 #include <asm/set_memory.h>
 #include <asm/tlbflush.h>
 
-/* The alignment should be at least a 4K page. */
-#define __kfence_pool_attrs __aligned(PAGE_SIZE)
+extern bool kfence_alloc_pool(void);
 
 /*
  * The page fault handler entry function, up to which the stack trace is
@@ -24,6 +23,9 @@
 static inline bool arch_kfence_initialize_pool(void)
 {
 	unsigned long addr;
+
+	if (!kfence_alloc_pool())
+		return false;
 
 	for (addr = (unsigned long)__kfence_pool; is_kfence_address((void *)addr);
 	     addr += PAGE_SIZE) {
