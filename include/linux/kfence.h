5b4309d64a61f67bf9d646a6ce4055e30b14a21f
Marco Elver <elver@google.com>
2020-10-14 19:28:17 +0200

    kfence: core: Switch to using memblock allocations
    Use memblock allocator before SL*B is initialized, allowing us to
    allocate more objects that with page_alloc.
    
    Signed-off-by: Marco Elver <elver@google.com>

--- include/linux/kfence.h
+++ include/linux/kfence.h
@@ -43,7 +43,15 @@ static __always_inline bool is_kfence_address(const void *addr)
 }
 
 /**
+ * kfence_alloc_pool() - allocate the KFENCE pool via memblock
+ */
+void kfence_alloc_pool(void);
+
+/**
  * kfence_init() - perform KFENCE initialization at boot time
+ *
+ * Requires that kfence_alloc_pool() was called before. This sets up the
+ * allocation gate timer, and requires that workqueues are available.
  */
 void kfence_init(void);
 
@@ -161,6 +169,7 @@ bool __must_check kfence_handle_page_fault(unsigned long addr);
 #else /* CONFIG_KFENCE */
 
 static inline bool is_kfence_address(const void *addr) { return false; }
+static inline void kfence_alloc_pool(void) { }
 static inline void kfence_init(void) { }
 static inline bool __must_check kfence_shutdown_cache(struct kmem_cache *s) { return true; }
 static inline void *kfence_alloc(struct kmem_cache *s, size_t size, gfp_t flags) { return NULL; }
