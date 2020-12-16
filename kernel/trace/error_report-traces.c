4e96701fe72753cb85dc7f047847ad1265504896
Alexander Potapenko <glider@google.com>
2020-12-16 15:57:33 +0100

    tracing: add error_report trace points
    Introduce error_report_start and error_report_end tracepoints.
    Those can be used in debugging tools like KASAN, KFENCE, etc.
    to provide extensions to the error reporting mechanisms (e.g. allow
    tests hook into error reporting, ease error report collection from
    production kernels).
    Another benefit would be making use of ftrace for debugging or
    benchmarking the tools themselves.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>

---
+++ kernel/trace/error_report-traces.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Error reporting trace points
+ */
+
+#define CREATE_TRACE_POINTS
+#include <trace/events/error_report.h>
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(error_report_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(error_report_end);
+
