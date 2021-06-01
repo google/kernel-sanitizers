88789974a2624fee74f2258a413c56f7fdd3968e
Marco Elver <elver@google.com>
2021-06-01 19:03:29 +0200

    data-race in lookup_fast / vfs_unlink
    read-write to 0xffff88800a7b20c0 of 4 bytes by task 3971 on cpu 2:
     dont_mount include/linux/dcache.h:358 [inline]
     vfs_unlink+0x281/0x3f0 fs/namei.c:4027
     do_unlinkat+0x238/0x4f0 fs/namei.c:4092
     __do_sys_unlink fs/namei.c:4139 [inline]
     __se_sys_unlink fs/namei.c:4137 [inline]
     __x64_sys_unlink+0x2c/0x30 fs/namei.c:4137
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    read to 0xffff88800a7b20c0 of 4 bytes by task 1081 on cpu 1:
     d_revalidate fs/namei.c:826 [inline]
     lookup_fast+0x15f/0x350 fs/namei.c:1578
     walk_component+0x5c/0x350 fs/namei.c:1938
     lookup_last fs/namei.c:2397 [inline]
     path_lookupat+0x11d/0x560 fs/namei.c:2421
     filename_lookup+0xff/0x390 fs/namei.c:2454
     user_path_at_empty+0x3b/0x50 fs/namei.c:2734
     do_readlinkat+0x87/0x200 fs/stat.c:425
     __do_sys_readlink fs/stat.c:458 [inline]
     __se_sys_readlink fs/stat.c:455 [inline]
     __x64_sys_readlink+0x43/0x50 fs/stat.c:455
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    value changed: 0x00600008 -> 0x00008008
    
    Signed-off-by: Marco Elver <elver@google.com>

--- fs/namei.c
+++ fs/namei.c
@@ -823,7 +823,7 @@ out_dput:
 
 static inline int d_revalidate(struct dentry *dentry, unsigned int flags)
 {
-	if (unlikely(dentry->d_flags & DCACHE_OP_REVALIDATE))
+	if (unlikely(data_race(dentry->d_flags & DCACHE_OP_REVALIDATE)))
 		return dentry->d_op->d_revalidate(dentry, flags);
 	else
 		return 1;
