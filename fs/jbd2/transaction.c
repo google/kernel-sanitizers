ca30e0080a09145c9e32233235ded62625262c6b
Marco Elver <elver@google.com>
2021-06-01 18:46:42 +0200

    data-race in jbd2_journal_dirty_metadata / jbd2_journal_dirty_metadata
    write to 0xffff88800a140d34 of 4 bytes by task 19514 on cpu 3:
     jbd2_journal_dirty_metadata+0x317/0x710 fs/jbd2/transaction.c:1521
     __ext4_handle_dirty_metadata+0xc6/0x590 fs/ext4/ext4_jbd2.c:331
     ext4_do_update_inode fs/ext4/inode.c:5139 [inline]
     ext4_mark_iloc_dirty+0x130b/0x1730 fs/ext4/inode.c:5724
     __ext4_mark_inode_dirty+0x4d2/0x5a0 fs/ext4/inode.c:5918
     ext4_ext_tree_init+0x7e/0x90 fs/ext4/extents.c:828
     __ext4_new_inode+0x2a11/0x2f80 fs/ext4/ialloc.c:1328
     ext4_symlink+0x2c1/0x700 fs/ext4/namei.c:3463
     vfs_symlink+0x23a/0x300 fs/namei.c:4172
     do_symlinkat+0xeb/0x280 fs/namei.c:4202
     __do_sys_symlink fs/namei.c:4223 [inline]
     __se_sys_symlink fs/namei.c:4221 [inline]
     __x64_sys_symlink+0x32/0x40 fs/namei.c:4221
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    read to 0xffff88800a140d34 of 4 bytes by task 19507 on cpu 2:
     jbd2_journal_dirty_metadata+0x191/0x710 fs/jbd2/transaction.c:1489
     __ext4_handle_dirty_metadata+0xc6/0x590 fs/ext4/ext4_jbd2.c:331
     ext4_do_update_inode fs/ext4/inode.c:5139 [inline]
     ext4_mark_iloc_dirty+0x130b/0x1730 fs/ext4/inode.c:5724
     __ext4_mark_inode_dirty+0x4d2/0x5a0 fs/ext4/inode.c:5918
     ext4_ext_tree_init+0x7e/0x90 fs/ext4/extents.c:828
     __ext4_new_inode+0x2a11/0x2f80 fs/ext4/ialloc.c:1328
     ext4_symlink+0x2c1/0x700 fs/ext4/namei.c:3463
     vfs_symlink+0x23a/0x300 fs/namei.c:4172
     do_symlinkat+0xeb/0x280 fs/namei.c:4202
     __do_sys_symlink fs/namei.c:4223 [inline]
     __se_sys_symlink fs/namei.c:4221 [inline]
     __x64_sys_symlink+0x32/0x40 fs/namei.c:4221
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    value changed: 0x00000000 -> 0x00000001
    
    Signed-off-by: Marco Elver <elver@google.com>

--- fs/jbd2/transaction.c
+++ fs/jbd2/transaction.c
@@ -1486,7 +1486,7 @@ int jbd2_journal_dirty_metadata(handle_t *handle, struct buffer_head *bh)
 				jh->b_next_transaction == transaction);
 		spin_unlock(&jh->b_state_lock);
 	}
-	if (jh->b_modified == 1) {
+	if (READ_ONCE(jh->b_modified) == 1) {
 		/* If it's in our transaction it must be in BJ_Metadata list. */
 		if (data_race(jh->b_transaction == transaction &&
 		    jh->b_jlist != BJ_Metadata)) {
@@ -1518,7 +1518,7 @@ int jbd2_journal_dirty_metadata(handle_t *handle, struct buffer_head *bh)
 			ret = -ENOSPC;
 			goto out_unlock_bh;
 		}
-		jh->b_modified = 1;
+		WRITE_ONCE(jh->b_modified, 1);
 		handle->h_total_credits--;
 	}
 
