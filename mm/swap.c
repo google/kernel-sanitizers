1f9f7144187ba37f3040012507031bf12cf641be
Marco Elver <elver@google.com>
2021-06-01 19:30:18 +0200

    data-race in __lru_add_drain_all / lru_cache_add_inactive_or_unevictable
    read-write to 0xffff88807dca64a8 of 1 bytes by task 2249 on cpu 1:
     pagevec_add include/linux/pagevec.h:74 [inline]
     pagevec_add_and_need_flush mm/swap.c:244 [inline]
     lru_cache_add mm/swap.c:474 [inline]
     lru_cache_add_inactive_or_unevictable+0xa2/0x270 mm/swap.c:506
     do_anonymous_page+0x6ff/0x8a0 mm/memory.c:3628
     handle_pte_fault mm/memory.c:4365 [inline]
     __handle_mm_fault mm/memory.c:4502 [inline]
     handle_mm_fault+0x95c/0x1a80 mm/memory.c:4600
     do_user_addr_fault+0x60c/0xc00 arch/x86/mm/fault.c:1390
     handle_page_fault arch/x86/mm/fault.c:1475 [inline]
     exc_page_fault+0x94/0x230 arch/x86/mm/fault.c:1531
     asm_exc_page_fault+0x1e/0x30 arch/x86/include/asm/idtentry.h:577
     copy_user_enhanced_fast_string+0xe/0x40 arch/x86/lib/copy_user_64.S:205
     copy_user_generic arch/x86/include/asm/uaccess_64.h:37 [inline]
     raw_copy_to_user arch/x86/include/asm/uaccess_64.h:58 [inline]
     _copy_to_user+0x77/0x90 lib/usercopy.c:33
     copy_to_user include/linux/uaccess.h:200 [inline]
     pagemap_read+0x2e6/0x450 fs/proc/task_mmu.c:1646
     do_loop_readv_writev fs/read_write.c:761 [inline]
     do_iter_read+0x3b1/0x4b0 fs/read_write.c:803
     vfs_readv fs/read_write.c:921 [inline]
     do_preadv+0x139/0x210 fs/read_write.c:1013
     __do_sys_preadv fs/read_write.c:1063 [inline]
     __se_sys_preadv fs/read_write.c:1058 [inline]
     __x64_sys_preadv+0x54/0x60 fs/read_write.c:1058
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    read to 0xffff88807dca64a8 of 1 bytes by task 2253 on cpu 2:
     pagevec_count include/linux/pagevec.h:61 [inline]
     __lru_add_drain_all+0x12c/0x3e0 mm/swap.c:828
     lru_add_drain_all+0xc/0x10 mm/swap.c:850
     invalidate_bdev+0x50/0x70 fs/block_dev.c:96
     ext4_put_super+0x5d0/0x8f0 fs/ext4/super.c:1230
     generic_shutdown_super+0xd2/0x220 fs/super.c:465
     kill_block_super+0x4a/0x90 fs/super.c:1395
     deactivate_locked_super+0x65/0xd0 fs/super.c:335
     deactivate_super+0x85/0x90 fs/super.c:366
     cleanup_mnt+0x267/0x2d0 fs/namespace.c:1136
     __cleanup_mnt+0x15/0x20 fs/namespace.c:1143
     task_work_run+0xae/0x130 kernel/task_work.c:164
     tracehook_notify_resume include/linux/tracehook.h:189 [inline]
     exit_to_user_mode_loop kernel/entry/common.c:174 [inline]
     exit_to_user_mode_prepare+0x149/0x180 kernel/entry/common.c:208
     __syscall_exit_to_user_mode_work kernel/entry/common.c:290 [inline]
     syscall_exit_to_user_mode+0x20/0x40 kernel/entry/common.c:301
     do_syscall_64+0x56/0x90 arch/x86/entry/common.c:57
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    value changed: 0x0b -> 0x0c
    
    Signed-off-by: Marco Elver <elver@google.com>

--- mm/swap.c
+++ mm/swap.c
@@ -825,7 +825,7 @@ inline void __lru_add_drain_all(bool force_all_cpus)
 		struct work_struct *work = &per_cpu(lru_add_drain_work, cpu);
 
 		if (force_all_cpus ||
-		    pagevec_count(&per_cpu(lru_pvecs.lru_add, cpu)) ||
+		    data_race(pagevec_count(&per_cpu(lru_pvecs.lru_add, cpu))) ||
 		    data_race(pagevec_count(&per_cpu(lru_rotate.pvec, cpu))) ||
 		    pagevec_count(&per_cpu(lru_pvecs.lru_deactivate_file, cpu)) ||
 		    pagevec_count(&per_cpu(lru_pvecs.lru_deactivate, cpu)) ||
