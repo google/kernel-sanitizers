98cf687f3f07ab7882961306a409a198fca0c7d0
Marco Elver <elver@google.com>
2021-06-03 16:14:35 +0200

    data-race in n_tty_receive_buf_common / n_tty_receive_buf_common
    write to 0xffffc900282e1020 of 8 bytes by task 9432 on cpu 3:
     flush_echoes drivers/tty/n_tty.c:825 [inline]
     __receive_buf drivers/tty/n_tty.c:1645 [inline]
     n_tty_receive_buf_common+0x1bf9/0x1e90 drivers/tty/n_tty.c:1740
     n_tty_receive_buf+0x2b/0x40 drivers/tty/n_tty.c:1769
     tiocsti drivers/tty/tty_io.c:2315 [inline]
     tty_ioctl+0x969/0x1120 drivers/tty/tty_io.c:2721
     vfs_ioctl fs/ioctl.c:51 [inline]
     __do_sys_ioctl fs/ioctl.c:1069 [inline]
     __se_sys_ioctl+0xcb/0x140 fs/ioctl.c:1055
     __x64_sys_ioctl+0x3f/0x50 fs/ioctl.c:1055
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    read to 0xffffc900282e1020 of 8 bytes by task 9510 on cpu 0:
     flush_echoes drivers/tty/n_tty.c:821 [inline]
     __receive_buf drivers/tty/n_tty.c:1645 [inline]
     n_tty_receive_buf_common+0x1b9f/0x1e90 drivers/tty/n_tty.c:1740
     n_tty_receive_buf+0x2b/0x40 drivers/tty/n_tty.c:1769
     tiocsti drivers/tty/tty_io.c:2315 [inline]
     tty_ioctl+0x969/0x1120 drivers/tty/tty_io.c:2721
     vfs_ioctl fs/ioctl.c:51 [inline]
     __do_sys_ioctl fs/ioctl.c:1069 [inline]
     __se_sys_ioctl+0xcb/0x140 fs/ioctl.c:1055
     __x64_sys_ioctl+0x3f/0x50 fs/ioctl.c:1055
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    value changed: 0x0000000000000012 -> 0x0000000000000015
    
    Signed-off-by: Marco Elver <elver@google.com>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -818,11 +818,11 @@ static void flush_echoes(struct tty_struct *tty)
 	struct n_tty_data *ldata = tty->disc_data;
 
 	if ((!L_ECHO(tty) && !L_ECHONL(tty)) ||
-	    ldata->echo_commit == ldata->echo_head)
+	    READ_ONCE(ldata->echo_commit) == ldata->echo_head)
 		return;
 
 	mutex_lock(&ldata->output_lock);
-	ldata->echo_commit = ldata->echo_head;
+	WRITE_ONCE(ldata->echo_commit, ldata->echo_head);
 	__process_echoes(tty);
 	mutex_unlock(&ldata->output_lock);
 }
