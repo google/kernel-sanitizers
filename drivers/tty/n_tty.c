4b0ae169d3dbf1f825037e788eaa140d4742a17f
Marco Elver <elver@google.com>
2021-06-03 12:37:12 +0200

    data-race in n_tty_poll / tty_set_termios
    write to 0xffff888042fa9d26 of 1 bytes by task 11385 on cpu 0:
     unset_locked_termios drivers/tty/tty_ioctl.c:257 [inline]
     tty_set_termios+0x3a2/0x870 drivers/tty/tty_ioctl.c:331
     set_termios+0x379/0x3b0 drivers/tty/tty_ioctl.c:414
     tty_mode_ioctl+0x587/0x690 drivers/tty/tty_ioctl.c:725
     n_tty_ioctl_helper+0x8b/0x4c0 drivers/tty/tty_ioctl.c:883
     n_tty_ioctl+0x102/0x200 drivers/tty/n_tty.c:2487
     tty_ioctl+0xa90/0x1120 drivers/tty/tty_io.c:2814
     vfs_ioctl fs/ioctl.c:51 [inline]
     __do_sys_ioctl fs/ioctl.c:1069 [inline]
     __se_sys_ioctl+0xcb/0x140 fs/ioctl.c:1055
     __x64_sys_ioctl+0x3f/0x50 fs/ioctl.c:1055
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    read to 0xffff888042fa9d26 of 1 bytes by task 9670 on cpu 2:
     input_available_p drivers/tty/n_tty.c:1934 [inline]
     n_tty_poll+0xb7/0x4a0 drivers/tty/n_tty.c:2430
     tty_poll+0xa7/0x140 drivers/tty/tty_io.c:2231
     vfs_poll include/linux/poll.h:90 [inline]
     do_select+0x8a4/0xf80 fs/select.c:534
     core_sys_select+0x353/0x600 fs/select.c:677
     kern_select fs/select.c:718 [inline]
     __do_sys_select fs/select.c:725 [inline]
     __se_sys_select+0x180/0x1c0 fs/select.c:722
     __x64_sys_select+0x63/0x70 fs/select.c:722
     do_syscall_64+0x4a/0x90 arch/x86/entry/common.c:47
     entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    value changed: 0x80 -> 0x46
    
    Signed-off-by: Marco Elver <elver@google.com>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -1931,7 +1931,7 @@ static int n_tty_open(struct tty_struct *tty)
 static inline int input_available_p(struct tty_struct *tty, int poll)
 {
 	struct n_tty_data *ldata = tty->disc_data;
-	int amt = poll && !TIME_CHAR(tty) && MIN_CHAR(tty) ? MIN_CHAR(tty) : 1;
+	int amt = data_race(poll && !TIME_CHAR(tty) && MIN_CHAR(tty) ? MIN_CHAR(tty) : 1);
 
 	if (ldata->icanon && !L_EXTPROC(tty))
 		return ldata->canon_head != ldata->read_tail;
