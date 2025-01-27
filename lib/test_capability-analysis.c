5708875d788af3d3ace0f98817e92ab0fa2e83b8
Marco Elver <elver@google.com>
2025-01-27 17:48:03 +0100

    bit_spinlock: Support Clang's capability analysis
    The annotations for bit_spinlock.h have simply been using "bitlock" as
    the token. For Sparse, that was likely sufficient in most cases. But
    Clang's capability analysis is more precise, and we need to ensure we
    can distinguish different bitlocks.
    
    To do so, add a token capability, and a macro __bitlock(bitnum, addr)
    that is used to construct unique per-bitlock tokens.
    
    Add the appropriate test.
    
    <linux/list_bl.h> is implicitly included through other includes, and
    requires 2 annotations to indicate that acquisition (without release)
    and release (without prior acquisition) of its bitlock is intended.
    
    Signed-off-by: Marco Elver <elver@google.com>

--- lib/test_capability-analysis.c
+++ lib/test_capability-analysis.c
@@ -4,6 +4,7 @@
  * positive errors when compiled with Clang's capability analysis.
  */
 
+#include <linux/bit_spinlock.h>
 #include <linux/build_bug.h>
 #include <linux/mutex.h>
 #include <linux/seqlock.h>
@@ -250,4 +251,29 @@ static void __used test_seqlock_writer(struct test_seqlock_data *d)
 	write_seqlock_irqsave(&d->sl, flags);
 	d->counter++;
 	write_sequnlock_irqrestore(&d->sl, flags);
+}
+
+struct test_bit_spinlock_data {
+	unsigned long bits;
+	int counter __guarded_by(__bitlock(3, &bits));
+};
+
+static void __used test_bit_spin_lock(struct test_bit_spinlock_data *d)
+{
+	/*
+	 * Note, the analysis seems to have false negatives, because it won't
+	 * precisely recognize the bit of the fake __bitlock() token.
+	 */
+	bit_spin_lock(3, &d->bits);
+	d->counter++;
+	bit_spin_unlock(3, &d->bits);
+
+	bit_spin_lock(3, &d->bits);
+	d->counter++;
+	__bit_spin_unlock(3, &d->bits);
+
+	if (bit_spin_trylock(3, &d->bits)) {
+		d->counter++;
+		bit_spin_unlock(3, &d->bits);
+	}
 }
